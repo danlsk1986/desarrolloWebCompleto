5a4109a1f3498a03fef19d3dd6ff373fb738ccb6
Daniel <danlsk1986@gmail.com>
2020-05-27 12:45:12 -0300

    Agregando variables del formulario

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 (function(){
     'use strict'
     document.addEventListener('DOMContentLoaded', function(){
-        console.log('LISTO');
+        
+        //Datos usuario
+        var nombre = document.getElementById('nombre');
+        var apellido = document.getElementById('apellido');
+        var email = document.getElementById('email');
+
+        
+        
+        //Campos pases
+
+        var pase_dia = document.getElementById('pase_dia');
+        var pase_completo = document.getElementById('pase_completo');
+        var pase_dosdias = document.getElementById('pase_dosdia');
+
+        //Botones y divs
+
+        var calcular = document.getElementById('calcular');
+        var errorDiv = document.getElementById('error');
+        var botonRegistro = document.getElementById('btnRegistro');
+        var resultado = document.getElementById('lista-productos');
+        
         
     });//DOM CONTENT LOADED
 
